9240c73d0b2409a5c45683037314d57b638922be
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-08-07 15:18:48 +0530

    custom domain

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,22 +1,28 @@
 // 'use client'
+import getDomain from '../lib/getDomain'
 
 async function getData() {
-  // const endpoint = 'http://localhost:3000/api/posts'
-  // s
-  // const res = await fetch(endpoint)
+  const domain = getDomain()
+  const endpoint = `${domain}/api/posts`
 
-  // if (!res.ok) {
-  //   throw new Error('Failed to fetch data')
-  // }
+  const res = await fetch(endpoint)
 
-  // return res.json()
-  return { items: [] }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  if (res.headers.get('content-type') !== 'application/json') {
+    return { items: [] }
+  }
+  return res.json()
+  // return { items: [] }
 }
 
 export default async function BlogPage() {
   const data = await getData()
   const items = data && data.items ? [...data.items] : []
   console.log(items)
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
   return (
     <main>
       <h1>Hello World</h1>
