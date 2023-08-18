cfccb5cba20712be60a93e02c54969bd7b966d30
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-08-18 23:37:45 +0530

    create db with drizzle kit migrations

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -6,6 +6,17 @@ export async function helloWorld() {
   const [dbResponse] = await sql`SELECT NOW();`
   const dbNow = dbResponse && dbResponse.now ? dbResponse.now : ''
   const end = new Date()
-  //   return { dbNow: dbNow, latency: Math.abs(end - start) }
   return { dbNow: dbNow, latency: Math.abs(end - start) }
 }
+
+async function configureDatabase() {
+  const dbResponse = await sql`CREATE TABLE IF NOT EXISTS "links" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"url" text NOT NULL,
+	"short" varchar(50),
+	"created_at" timestamp DEFAULT now()
+);
+`
+}
+
+configureDatabase().catch((err) => console.log('db config err', err))
